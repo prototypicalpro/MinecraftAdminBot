d8bea7ce49445ca7ac63caa55a788c413b470959
Noah Koontz <nkoontz@newrelic.com>
2020-07-16 10:43:37 -0700

    switch to .env secrets

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { ComputeManagementClient } = require('@azure/arm-compute');
 const client = new Discord.Client()
 const PREFIX = '/'
 
-const secrets = JSON.parse(require('fs').readFileSync('./secrets.json'))
+require('dotenv').config({ path: process.env.NODE_ENV === 'production' ? '~/.env' : null })
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`)
@@ -20,23 +20,23 @@ client.on('message', async message => {
         // admin commands!
         if (parsed.command === 'start' || parsed.command === 'stop' || parsed.command === 'status') {
             // authenticate to azure
-            const creds = await msRestAzure.loginWithServicePrincipalSecret(secrets.CLIENT_ID, secrets.CLIENT_SECRET, secrets.TENANT_ID)
-            const computeClient = new ComputeManagementClient(creds, secrets.SUB_ID)
+            const creds = await msRestAzure.loginWithServicePrincipalSecret(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.TENANT_ID)
+            const computeClient = new ComputeManagementClient(creds, process.env.SUB_ID)
             if (parsed.command === 'stop') {
                 message.reply('Powering off the VM (this may take a minute)...')
-                await computeClient.virtualMachines.deallocate(secrets.VM_RESOURCE_GROUP_NAME, secrets.VM_NAME)
+                await computeClient.virtualMachines.deallocate(process.env.VM_RESOURCE_GROUP_NAME, process.env.VM_NAME)
                 message.reply('Successfully Powered off the VM.')
             }
             if (parsed.command === 'start') {
                 message.reply('Powering on the VM (this may take a minute)...')
-                await computeClient.virtualMachines.start(secrets.VM_RESOURCE_GROUP_NAME, secrets.VM_NAME)
+                await computeClient.virtualMachines.start(process.env.VM_RESOURCE_GROUP_NAME, process.env.VM_NAME)
                 message.reply('Successfully Powered on the VM.')
             }
-            const vm = await computeClient.virtualMachines.get(secrets.VM_RESOURCE_GROUP_NAME, secrets.VM_NAME, { expand: 'instanceView' })
+            const vm = await computeClient.virtualMachines.get(process.env.VM_RESOURCE_GROUP_NAME, process.env.VM_NAME, { expand: 'instanceView' })
             const status = vm.instanceView.statuses.find(s => s.code.includes('PowerState')).code
             message.reply(`VM is currently ${status.split('/')[1]}`)
         }
     }
 })
 
-client.login(secrets.DISCORD_SECRET)
+client.login(process.env.DISCORD_SECRET)
